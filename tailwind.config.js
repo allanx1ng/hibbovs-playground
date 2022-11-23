05df82ba29ff55693b0258808deee046712be1c4
allanx1ing <allanxing2003@gmail.com>
2022-11-22 16:31:17 -0800

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
